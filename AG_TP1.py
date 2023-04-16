f83c7b69f2cee508eb622dab51278c1eb0cba8b3
Ramiro <ariasrasmirox@gmail.com>
2023-04-16 20:19:21 -0300

    Cambios en main

--- AG_TP1.py
+++ AG_TP1.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import os
 import math
 import random as r
@@ -11,17 +11,18 @@ class Constant:
     CICLOS_PROGRAMA     = 20
     COEF                = (2**30) -1
 
-def main():
-    binario_COEF = decimal_a_binario(Constant.COEF)
-    cant_Genes = len(binario_COEF)
-
+def poblacion_inicial():
+    array_poblacion_inicial = []
+    for i in range(Constant.POBLACION_INICIAL):
+        randInt = r.randint(0,Constant.COEF)
+        array_poblacion_inicial.append(randInt)
+    return array_poblacion_inicial
 
 def evaluar(x):
     return (x/Constant.COEF)**2
 
-
 def correr(conjunto_solucion,cant_corridas):
-    subconjunto_poblacion = poblacion_inicial(conjunto_solucion)
+    subconjunto_poblacion = poblacion_inicial()
     maximos = []
     minimos = []
     promedios = []
@@ -32,15 +33,6 @@ def correr(conjunto_solucion,cant_corridas):
         maximos.append(np.max(resultado))
         minimos.append(np.min(resultado))
         promedios.append(np.average(resultado))
-
-
-def poblacion_inicial(conjunto_solucion):
-    array_poblacion_inicial = []
-    for i in range(Constant.POBLACION_INICIAL):
-        randint = r.randint(0,Constant.COEF)
-        array_poblacion_inicial.append(conjunto_solucion[randint])
-        return array_poblacion_inicial
-
 
 def decimal_a_binario(decimal):
     if decimal <= 0:
@@ -58,7 +50,6 @@ def decimal_a_binario(decimal):
     print(f"El número {decimal} es {binario} en binario")
     return binario
 
-
 def binario_a_decimal(binario):
     posicion = 0
     decimal = 0
@@ -70,3 +61,12 @@ def binario_a_decimal(binario):
         decimal += int(digito) * multiplicador
         posicion += 1
     return decimal
+
+def main():
+    #binario_COEF = decimal_a_binario(Constant.COEF)
+    #cant_Genes = len(binario_COEF)
+    print("Hola mundo")
+    print(poblacion_inicial())
+
+if __name__ == "__main__":
+    main()

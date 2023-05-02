9279293aec3bad2c5064a61a072d158eba22ae6e
victoria-peinado <49839433+victoria-peinado@users.noreply.github.com>
2023-05-01 21:08:28 -0300

    mutacion
    Agregada mutacion. Por alguna razon me da un error en la linea 16 si pongo P_MUTACION

--- AG_TP1.py
+++ AG_TP1.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib as plt
+#import matplotlib as plt
 import os
 import math
 import random as r
@@ -10,6 +10,21 @@ class Constant:
     POBLACION_INICIAL   = 10
     CICLOS_PROGRAMA     = 20
     COEF                = (2**30) -1
+    
+def mutacion(binarioList):
+    for i in range(len(binarioList)):
+        if r.random()< 0.05: 
+            binarioList[i] = str(1 - int(binarioList[i]))
+    return(binarioList)
+    
+def mutar_poblacion(poblacion):
+    poblacion_final=[]
+    for i in range(len(poblacion)):
+        cromosoma_binario=list(np.binary_repr(poblacion[i],30)) #pasa el cormosoma a binario
+        mutacion(cromosoma_binario)
+        cromosoma_decimal=int(''.join(cromosoma_binario),2)#pasa el cromosoma a decimal
+        poblacion_final.append(cromosoma_decimal)#agrega el cromosoma a la nueva poblacion
+    print((poblacion_final))
 
 def poblacion_inicial():
     array_poblacion_inicial = []
@@ -34,17 +49,12 @@ def correr(conjunto_solucion,cant_corridas):
         minimos.append(np.min(resultado))
         promedios.append(np.average(resultado))
 
-#bin(n) decimal -> binario
-#int(n,2) binario->decimal
-#[int(i) for i in str(bin(n))[2:]] binario-> list
-#int("".join([str(i) for i in lst]),2) lista(binaria) -> decimal 
-#random.choices(lista,prob, k=2)   ruleta de dos elementos de una lista dadas sus prob
-
 def main():
     #binario_COEF = decimal_a_binario(Constant.COEF)
     #cant_Genes = len(binario_COEF)
-    print("Hola mundo")
-    print(poblacion_inicial())
+    a=poblacion_inicial()
+    print(a)
+    mutar_poblacion(a)
 
 if __name__ == "__main__":
     main()

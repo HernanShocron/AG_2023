bcbb8537790f4ebd216e7f531747f00a55be26f9
HernanShocron <hernanshocron@hotmail.com>
2023-10-26 00:20:00 -0300

    Heurística TERMINADA
    Logre terminar la lógica de heurística. funciona bien. incluso, aplique la lógica para que genere un excel en la carpeta raíz del proyecto. Dicho excel colorea los lugares por los que pasó el viajante. Por alguna razón cuando itero la generación me colorea en rojo la ciudad de partida seleccionada en la iteración anterior.

--- TP3/ProblemaViajante.py
+++ TP3/ProblemaViajante.py
@@ -40,36 +40,48 @@ Descripcion_Recorrido = ''
 Id_Ciudad_Actual = 0
 Id_Ciudad_Cercana = 0
 
-class Constant :
+cwd             = os.getcwd()
+FileName        = 'Distancias_Ruta.xlsx'
+ExcelDocument   = Excel.load_workbook(cwd+'\TP3\ '.strip() +FileName)  # Strip es para quitar los espacios en blanco, python no me deja poner la barra invertida al final de la cadena
+Sheet           = ExcelDocument.get_sheet_by_name('PorRuta')
+
+
+class Constant : 
     CIUDADES = np.array((
-        [ 1,'Córdoba'                               ],
-        [ 2,'Corrientes'                            ],
-        [ 3,'Formosa'                               ],
-        [ 4,'La Plata'                              ],
-        [ 5,'La Rioja'                              ],
-        [ 6,'Mendoza'                               ],
-        [ 7,'Neuquén'                               ],
-        [ 8,'Paraná'                                ],
-        [ 9,'Posadas'                               ],
-        [10,'Rawson'                                ],
-        [11,'Resistencia'                           ],
-        [12,'Río Gallegos'                          ],
-        [13,'San Fernando del Valle de Catamarca'   ],
-        [14,'San Miguel de Tucumán'                 ],
-        [15,'Salta'                                 ],
-        [16,'San Juan'                              ],
-        [17,'San Luis'                              ],
-        [18,'San Salvador de Jujuy'                 ],
-        [19,'Santa Fe'                              ],
-        [20,'Santa Rosa'                            ],
-        [21,'Santiago del Estero'                   ],
-        [22,'Ushuaia'                               ],
-        [23,'Viedma'                                ]))
-    cwd             = os.getcwd()
-    ExcelDocument   = Excel.load_workbook(cwd+'\TP3\Distancias_Ruta.xlsx')
-    Sheet           = ExcelDocument.get_sheet_by_name('PorRuta')
+        ['A','Córdoba'                               ],
+        ['B','Corrientes'                            ],
+        ['C','Formosa'                               ],
+        ['D','La Plata'                              ],
+        ['E','La Rioja'                              ],
+        ['F','Mendoza'                               ],
+        ['G','Neuquén'                               ],
+        ['H','Paraná'                                ],
+        ['I','Posadas'                               ],
+        ['J','Rawson'                                ],
+        ['K','Resistencia'                           ],
+        ['L','Río Gallegos'                          ],
+        ['M','San Fernando del Valle de Catamarca'   ],
+        ['N','San Miguel de Tucumán'                 ],
+        ['O','Salta'                                 ],
+        ['P','San Juan'                              ],
+        ['Q','San Luis'                              ],
+        ['R','San Salvador de Jujuy'                 ],
+        ['S','Santa Fe'                              ],
+        ['T','Santa Rosa'                            ],
+        ['U','Santiago del Estero'                   ],
+        ['V','Ushuaia'                               ],
+        ['W','Viedma'                                ]))
+    
 
 def main():
+    global cwd
+    global FileName
+    global ExcelDocument
+    global Sheet
+
+    ExcelDocument   = Excel.load_workbook(cwd+'\TP3\ '.strip() +FileName)  # Strip es para quitar los espacios en blanco, python no me deja poner la barra invertida al final de la cadena
+    Sheet           = ExcelDocument.get_sheet_by_name('PorRuta')
+    
     OPC = 0
     while OPC == 0:
         system("cls")
@@ -79,6 +91,7 @@ def main():
         print('')
         OPC = int(input('OPCIÓN: '))
         if OPC > 2 or OPC < 0:
+            print('Opción Incorrecta, vuelva a intentar...')
             OPC = 0
     match OPC:
         case 1:
@@ -91,6 +104,7 @@ def main():
                 print('')
                 OPC2 = int(input('OPCIÓN: '))
                 if OPC2 > 2 or OPC2 < 0:
+                    print('Opción Incorrecta, vuelva a intentar...')
                     OPC2 = 0
             match OPC2:
                 case 1:
@@ -99,25 +113,48 @@ def main():
                         system("cls")
                         print('Seleccione Ciudad Inicial:')
                         print('')
-                        for ciudad in Constant.CIUDADES:
-                            print(str(ciudad[0])+' - '+ciudad[1])
+                        ContCiudad = 0
+                        for Ciudad in Constant.CIUDADES:
+                            ContCiudad+=1
+                            print(str(ContCiudad)+' - '+Ciudad[1])
                         print('')
                         OPC3 = int(input('Seleccione Opción: '))
                         if OPC3 > 23 or OPC3 < 0:
+                            print('Opción Incorrecta, vuelva a intentar...')
                             OPC3 = 0
                         else:
                             Heuristico_Con_Ciudad(OPC3)
+                            ExcelDocument.save(Constant.CIUDADES[Id_Ciudad_Actual-1][1]+'_'+FileName)
+                            ExcelDocument.close()                          
                 case 2:
                     Heuristico_Sin_Ciudad()
+                    ExcelDocument.save(Constant.CIUDADES[Id_Ciudad_Actual-1][1]+'_'+FileName)
+                    ExcelDocument.close()
         case 2:
             Algoritmo_Genetico()
-    Constant.ExcelDocument.close()
-    
+    Volver_Menu()
+
+def Volver_Menu():
+    OPC = 0
+    while OPC == 0:
+        print('¿ Desea volver al menú ?')
+        print('    1-SI   2-NO')
+        OPC = int(input('OPCIÓN: '))
+        if OPC > 2 or OPC < 0:
+            print('Opción Incorrecta, vuelva a intentar...')
+            OPC = 0
+    match OPC:
+        case 1:
+            main()
+        case 2:
+            system("cls")
+            print('Ejecución Finalizada!')
+
 def Heuristico_Con_Ciudad(Id_Ciudad_Seleccionada):
     system("cls")
     print('Heuristico_Con_Ciudad')
     print('')
-    print('Id_Ciudad_Seleccionada = '+str(Id_Ciudad_Seleccionada))
+    print('Id_Ciudad_Seleccionada = '+str(Id_Ciudad_Seleccionada)+' - '+Constant.CIUDADES[Id_Ciudad_Seleccionada-1][1])
     Heuristica(Id_Ciudad_Seleccionada)
 
 def Heuristico_Sin_Ciudad():
@@ -125,9 +162,9 @@ def Heuristico_Sin_Ciudad():
     print('Heuristico_Sin_Ciudad')
     print('')
     Id_Ciudad_Random = r.randint(1,23)
-    print('Id_Ciudad_Random = '+str(Id_Ciudad_Random))
-    Heuristica(Id_Ciudad_Random)
     
+    print('Id_Ciudad_Random = '+str(Id_Ciudad_Random)+' - '+Constant.CIUDADES[Id_Ciudad_Random-1][1])
+    Heuristica(Id_Ciudad_Random)    
 
 def Heuristica(Id_Ciudad_Inicial):
     global Ciudades_Disponibles
@@ -135,57 +172,67 @@ def Heuristica(Id_Ciudad_Inicial):
     global Id_Ciudad_Cercana
     global Descripcion_Recorrido
     global Distancia_Recorrida
+
     Distancia_Recorrida = 0
     Descripcion_Recorrido = ''
-    Id_Ciudad_Actual = Id_Ciudad_Inicial
+    my_red      = Excel.styles.colors.Color(rgb='00FF0000')
+    my_green    = Excel.styles.colors.Color(rgb='0000FF00')
+    fillRed     = Excel.styles.fills.PatternFill(patternType='solid', fgColor=my_red)
+    fillGreen   = Excel.styles.fills.PatternFill(patternType='solid', fgColor=my_green)
+
     for i in range(23):
         Ciudades_Disponibles.append(i+1)
-    Ciudades_Disponibles.remove(Id_Ciudad_Actual)
+    Ciudades_Disponibles.remove(Id_Ciudad_Inicial)
     Distancia_Recorrida   = 0
     Descripcion_Recorrido = ''
+    Id_Ciudad_Actual = Id_Ciudad_Inicial
     Id_Ciudad_Cercana = Ciudad_Cercana_Disponible(Id_Ciudad_Actual)
     for i in range(22):
-        Viajar_a()
+        Distancia_Recorrida     += Sheet.cell(row=Id_Ciudad_Actual,column=Id_Ciudad_Cercana).value
+        #print('PINTAR ROJO'+str(Id_Ciudad_Actual)+ '-' + str(Id_Ciudad_Cercana))
+        Sheet.cell(row=Id_Ciudad_Actual,column=Id_Ciudad_Cercana).fill = fillRed
+        Descripcion_Recorrido   += '('+Constant.CIUDADES[Id_Ciudad_Actual-1][0]+') ' + Constant.CIUDADES[Id_Ciudad_Actual-1][1] + ' -> '
         Id_Ciudad_Actual = Id_Ciudad_Cercana
         Id_Ciudad_Cercana = Ciudad_Cercana_Disponible(Id_Ciudad_Actual)
     Id_Ciudad_Cercana = Id_Ciudad_Inicial
     Ciudades_Disponibles.append(Id_Ciudad_Inicial)
-    print('Viajes de regreso------------------> '+str(Id_Ciudad_Cercana))
-    print(Ciudades_Disponibles)
-    Viajar_a()
-    Descripcion_Recorrido = Descripcion_Recorrido[:-1]
-    print('El recorrido '+Descripcion_Recorrido+' tiene un total de '+str(Distancia_Recorrida)+' Kms')
+    #print('Viajes de regreso------------------> '+str(Id_Ciudad_Cercana))
+    #print(Ciudades_Disponibles)
+    Distancia_Recorrida     += Sheet.cell(row=Id_Ciudad_Actual,column=Id_Ciudad_Cercana).value
+    Sheet.cell(row=Id_Ciudad_Actual,column=Id_Ciudad_Cercana).fill = fillRed
+    Descripcion_Recorrido   += '('+Constant.CIUDADES[Id_Ciudad_Actual-1][0]+') ' + Constant.CIUDADES[Id_Ciudad_Actual-1][1] + ' -> '
+    Id_Ciudad_Actual = Id_Ciudad_Inicial
+    Descripcion_Recorrido   += '('+Constant.CIUDADES[Id_Ciudad_Actual-1][0]+') ' + Constant.CIUDADES[Id_Ciudad_Actual-1][1]
+    #print('PINTAR VERDE '+str(Id_Ciudad_Inicial)+ '-' + str(Id_Ciudad_Inicial))
+    Sheet.cell(row=Id_Ciudad_Inicial,column=Id_Ciudad_Inicial).fill = fillGreen
+    print('RECORRIDO: '+Descripcion_Recorrido)
+    print('')
+    print('TOTAL KMS Recorridos: '+str(Distancia_Recorrida)+' Kms')
+    print('')
 
 def Algoritmo_Genetico():
     system("cls")
     print('Algoritmo_Genetico')
     print('')
+    # Acá a diferencia de los TPs anteriores, en donde usabamos 1 o 0, vamos a tener que orientar el algoritmo a cromosomas del tipo ABCDEFGHIJKLMNOPQRSTUVW 
+    # (23 LETRAS, Una para cada ciduad) ya que es importante saber el orden de como visita las ciudades
 
 def Ciudad_Cercana_Disponible(Ciudad_Inicial):
     global Ciudades_Disponibles
     Distancia_Menor = 10000000
     Ciudad_Mas_Cercana = 0
-    print('La ciduad Inicial es '+str(Ciudad_Inicial))
+    #print('La ciduad Inicial es '+str(Ciudad_Inicial))
     for Ciudad in Ciudades_Disponibles:
-        print('La ciudad '+str(Ciudad)+' se encuentra disponible')
-        Distancia = Constant.Sheet.cell(row = Ciudad_Inicial, column = Ciudad).value
+        #print('La ciudad '+str(Ciudad)+' se encuentra disponible')
+        Distancia = Sheet.cell(row = Ciudad_Inicial, column = Ciudad).value
         if Distancia < Distancia_Menor:
             Distancia_Menor = Distancia
             Ciudad_Mas_Cercana = Ciudad
-    Ciudades_Disponibles.remove(Ciudad_Mas_Cercana)
-    print('Para '+str(Ciudad_Inicial)+' la ciudad más cercana es '+str(Ciudad_Mas_Cercana)+' con una distancia de '+str(Distancia_Menor)+' Kms')
+   # print('remove '+str(Ciudad_Mas_Cercana))
+    if Ciudad_Mas_Cercana != 0 :
+        Ciudades_Disponibles.remove(Ciudad_Mas_Cercana)
+    #print('Para '+str(Ciudad_Inicial)+' la ciudad más cercana es '+str(Ciudad_Mas_Cercana)+' con una distancia de '+str(Distancia_Menor)+' Kms')
     return Ciudad_Mas_Cercana
-
-def Viajar_a():
-    global Id_Ciudad_Actual
-    global Id_Ciudad_Cercana
-    global Distancia_Recorrida
-    global Descripcion_Recorrido
-    print('Id_Ciudad_Partida = '+str(Id_Ciudad_Actual) + ' Id_Ciudad_Destino = '+str(Id_Ciudad_Cercana))
-    Distancia_Recorrida     += Constant.Sheet.cell(row=Id_Ciudad_Actual,column=Id_Ciudad_Cercana).value
-    Descripcion_Recorrido   += Constant.CIUDADES[Id_Ciudad_Actual-1][0] + '-'
-    print('Distancia_Recorrida = '+str(Distancia_Recorrida))
-    print('Descripcion_Recorrido = '+Descripcion_Recorrido)
 
 if __name__ == "__main__":
     main()
